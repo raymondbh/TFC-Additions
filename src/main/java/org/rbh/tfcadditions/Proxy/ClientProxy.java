2c63b1ab54b91a450285ba010607be6249c031b6
raymondbh <raymondbh@gmail.com>
2015-08-25 20:30:22 +0200

    Fixed - key binding crash. TODO - fix localized name.

--- src/main/java/org/rbh/tfcadditions/Proxy/ClientProxy.java
+++ src/main/java/org/rbh/tfcadditions/Proxy/ClientProxy.java
@@ -1,11 +1,15 @@
 package org.rbh.tfcadditions.Proxy;
 
+import com.bioxx.tfc.api.Util.KeyBindings;
 import com.cricketcraft.ctmlib.CTMRenderer;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.client.settings.KeyBinding;
 import org.rbh.tfcadditions.Handlers.KeyBindingHandler;
 import org.rbh.tfcadditions.Utility.CarpentersBlocksHandler;
 import org.rbh.tfcadditions.Utility.NotEnoughItemsHandler;
@@ -20,6 +24,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void preInit(FMLPreInitializationEvent event){
         super.preInit(event);
+        registerKeys();
         registerKeyBindingHandler();
     }
 
@@ -40,6 +45,23 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void postInit(FMLPostInitializationEvent event){
         super.postInit(event);
+    }
+
+    public void registerKeys() {
+        KeyBindings.addKeyBinding(KeyBindingHandler.Key_PrevChiselMode);
+        KeyBindings.addIsRepeating(false);
+        uploadKeyBindingsToGame();
+    }
+
+    public void uploadKeyBindingsToGame()
+    {
+        GameSettings settings = Minecraft.getMinecraft().gameSettings;
+        KeyBinding[] tfcKeyBindings = KeyBindings.gatherKeyBindings();
+        KeyBinding[] allKeys = new KeyBinding[settings.keyBindings.length + tfcKeyBindings.length];
+        System.arraycopy(settings.keyBindings, 0, allKeys, 0, settings.keyBindings.length);
+        System.arraycopy(tfcKeyBindings, 0, allKeys, settings.keyBindings.length, tfcKeyBindings.length);
+        settings.keyBindings = allKeys;
+        settings.loadOptions();
     }
 
     public void registerKeyBindingHandler()
